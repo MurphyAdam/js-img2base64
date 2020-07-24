2e60d7a1682be651bb37063c938aef2c2d5beee0
Adam Clark <elm.majidi@gmail.com>
2020-07-24 19:17:19 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# js-img2base64
+Converts an image to Base64. You could pass a maximum size of an image, and an array of valid extensions. This package has no external dependecy packages.
+```js
+var imageToBase64 = require('js-img2base64');
+```
+
+Or
+
+```js
+import imageToBase64 from 'js-img2base64';
+```
+
+## install
+```
+npm install js-img2base64 --save
+```
+
+## API
+### imageToBase64(imageFile, maxSizeInMegabytes, validExtenions)
+
+Convert image file to image base64 data
+* {Image} ``imageFile``, required  
+* {number} optional, default <= 3 megabytes  
+* {array} optional, default ['png', 'jpeg', 'jpg', 'gif']
+```js
+
+    imageToBase64(image, 6, ["png", "gif"])
+    	.then((base64String) => console.log(base64String))
+    	.catch((error) => console.error(error));
+```
+
+### Usage example with React
+
+```js
+import React from 'react';
+import imageToBase64 from 'js-img2base64';
+
+
+const ImageComponent = () => {
+
+  const handleChange = event => {
+    const value = event.target.files[0];
+    imageToBase64(value)
+    	.then((base64String) => console.log(base64String))
+    	.catch((error) => console.error(error));
+  }
+
+  	return (
+      <input accept="image/*" 
+        onChange={handleChange} 
+        id="button-file" 
+        type="file" />
+  	);
+};
+
+```
+
+### Github repository
+
+[https://github.com/MurphyAdam/js-img2base64.git](https://github.com/MurphyAdam/js-img2base64.git)
---
+++ index.js
@@ -0,0 +1,47 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+function isValidImage(image, maxSizeInMegabytes, validExtenions) {
+  var extArray = ['png', 'jpeg', 'jpg', 'gif'];
+  if (validExtenions && validExtenions.length) extArray = validExtenions;
+  return new Promise(function (resolve, reject) {
+    if (extArray.indexOf(image.type.split('/')[1]) !== -1) {
+      var maxSizeInBytes = maxSizeInMegabytes * 1024 * 1024;
+      if (image.size <= maxSizeInBytes) resolve(true);else reject("Image ".concat(image.name, " is greater than ").concat(Math.round(maxSizeInBytes / 1024 / 1024), " megabytes."));
+    } else reject("Image ".concat(image.name, " is not a valid image format. (valid image formats are: ").concat(extArray.join(', '), ")"));
+  });
+}
+
+function getBase64Promise(file) {
+  return new Promise(function (resolve, reject) {
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onload = function () {
+      return resolve(reader.result);
+    };
+
+    reader.onerror = function (error) {
+      return reject(error);
+    };
+  });
+}
+
+function imageToBase64(image) {
+  var maxSizeInMegabytes = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 3;
+  var validExtenions = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : [];
+  return new Promise(function (resolve, reject) {
+    isValidImage(image, maxSizeInMegabytes, validExtenions).then(function () {
+      return resolve(getBase64Promise(image));
+    }).catch(function (error) {
+      return reject(error);
+    });
+  });
+}
+
+var _default = imageToBase64;
+exports.default = _default;
